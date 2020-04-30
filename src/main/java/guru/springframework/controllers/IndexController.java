1eb5ae8a7cb563197915ae9ea515d58bbda0bcc8
Christian Preß <Christian.Press@syracos.com>
2020-04-30 18:17:07 +0200

    Using Iteration with Fragments

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,10 +1,13 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Product;
 import guru.springframework.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -16,8 +19,9 @@ public class IndexController {
     }
 
     @RequestMapping("/")
-    public String getIndex(Model model){
-        model.addAttribute("products", productService.listProducts());
+    public String getIndex(Model model) {
+        List<Product> products = productService.listProducts();
+        model.addAttribute("products", products);
         return "index";
     }
 }
